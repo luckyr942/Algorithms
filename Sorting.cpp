178a2772f24bd488a01b5ea642d4720bc0ed0b86
Lucky Raj Choudhary <112571987+luckyr942@users.noreply.github.com>
2023-10-03 21:18:40 +0530

    Add files via upload

---
+++ Sorting.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+void SelectionSort(int arr[], int n){
+    for(int i=0; i<=n-2; i++){
+        int mini = i;
+        for(int j=i; j<=n-1; j++){
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp; 
+    }
+}
+void BubbleSort(int arr[], int n){
+    for(int i=n-1; i>=0; i--){
+        for(int j=0; j<=i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+void InsertionSort(int arr[], int n){
+    for(int i=0; i<=n-1; i++){
+        int j = i;
+        while(j >= 0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+
+            j--;
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    SelectionSort(arr,n);
+    BubbleSort(arr,n);
+    for(int i=0; i<n; i++){
+        cout<< arr[i] <<" ";
+    }
+    return 0;
+}
